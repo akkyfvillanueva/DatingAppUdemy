ff621c7d992247a6dbf0e63fb5632b5a8fad861d
Francisco Alejo Villanueva Vázquez <francisco.villanueva@akky.mx>
2021-11-08 14:05:17 -0600

    end of section 18

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -16,46 +16,44 @@ namespace API.Controllers
     [Authorize]
     public class MessagesController : BaseApiController
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IMessageRepository _messageRepository;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
-        public MessagesController(IUserRepository userRepository, IMessageRepository messageRepository,
+        public MessagesController(IUnitOfWork unitOfWork,
             IMapper mapper)
         {
-            _userRepository = userRepository;
-            _messageRepository = messageRepository;
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
         }
 
-        [HttpPost]
-        public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
-        {
-            var username = User.GetUserName();
+        // [HttpPost]
+        // public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+        // {
+        //     var username = User.GetUserName();
 
-            if (username == createMessageDto.RecipientUserName.ToLower())
-                return BadRequest("You cannot send a message to yourself");
+        //     if (username == createMessageDto.RecipientUserName.ToLower())
+        //         return BadRequest("You cannot send a message to yourself");
             
-            var sender = await _userRepository.GetUserByUserNameAsync(username);
-            var recipient = await _userRepository.GetUserByUserNameAsync(createMessageDto.RecipientUserName);
+        //     var sender = await _unitOfWork.UserRepository.GetUserByUserNameAsync(username);
+        //     var recipient = await _unitOfWork.UserRepository.GetUserByUserNameAsync(createMessageDto.RecipientUserName);
 
-            if (recipient == null) return NotFound();
+        //     if (recipient == null) return NotFound();
 
-            var message = new Message
-            {
-                Sender = sender,
-                Recipient = recipient,
-                SenderUserName = sender.UserName,
-                RecipientUserName = recipient.UserName,
-                Content = createMessageDto.Content,
-            };
+        //     var message = new Message
+        //     {
+        //         Sender = sender,
+        //         Recipient = recipient,
+        //         SenderUserName = sender.UserName,
+        //         RecipientUserName = recipient.UserName,
+        //         Content = createMessageDto.Content,
+        //     };
 
-            _messageRepository.AddMessage(message);
+        //     _unitOfWork.MessageRepository.AddMessage(message);
 
-            if (await _messageRepository.SaveAllAsync()) 
-                return Ok(_mapper.Map<MessageDto>(message));
+        //     if (await _unitOfWork.Complete()) 
+        //         return Ok(_mapper.Map<MessageDto>(message));
 
-            return BadRequest("Failed to send message");
-        }
+        //     return BadRequest("Failed to send message");
+        // }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser(
@@ -63,7 +61,7 @@ namespace API.Controllers
         {
             messageParams.UserName = User.GetUserName();
 
-            var messages = await _messageRepository.GetMessagesForUser(messageParams);
+            var messages = await _unitOfWork.MessageRepository.GetMessagesForUser(messageParams);
 
             Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize, 
                 messages.TotalCount, messages.TotalPages);
@@ -71,20 +69,12 @@ namespace API.Controllers
             return messages;
         }
 
-        [HttpGet("thread/{username}")]
-        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
-        {
-            var currentUserName = User.GetUserName();
-
-            return Ok(await _messageRepository.GetMessageThread(currentUserName, username));
-        }
-
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteMessage(int id)
         {
             var username = User.GetUserName();
 
-            var message = await _messageRepository.GetMessage(id);
+            var message = await _unitOfWork.MessageRepository.GetMessage(id);
 
             if (message.Sender.UserName != username && message.Recipient.UserName != username)
                 return Unauthorized();
@@ -94,9 +84,9 @@ namespace API.Controllers
             if (message.Recipient.UserName == username) message.RecipientDeleted = true;
 
             if (message.SenderDeleted && message.RecipientDeleted)
-                _messageRepository.DeleteMessage(message);
+                _unitOfWork.MessageRepository.DeleteMessage(message);
             
-            if (await _messageRepository.SaveAllAsync()) return Ok();
+            if (await _unitOfWork.Complete()) return Ok();
 
             return BadRequest("Problem deleting the message");
         }
--- API/SignalR/MessageHub.cs
+++ API/SignalR/MessageHub.cs
@@ -13,18 +13,16 @@ namespace API.SignalR
 {
     public class MessageHub : Hub
     {
-        private readonly IMessageRepository _messageRepository;
         private readonly IMapper _mapper;
-        private readonly IUserRepository _userRepository;
         private readonly IHubContext<PresenceHub> _presenceHub;
         private readonly PresenceTracker _tracker;
-        public MessageHub(IMessageRepository messageRepository, IMapper mapper,
-            IUserRepository userRepository, IHubContext<PresenceHub> presenceHub, 
+        private readonly IUnitOfWork _unitOfWork;
+        public MessageHub(IMapper mapper, IUnitOfWork unitOfWork,
+            IHubContext<PresenceHub> presenceHub, 
             PresenceTracker tracker)
         {
-            _messageRepository = messageRepository;
             _mapper = mapper;
-            _userRepository = userRepository;
+            _unitOfWork = unitOfWork;
             _presenceHub = presenceHub;
             _tracker = tracker;
         }
@@ -38,8 +36,10 @@ namespace API.SignalR
 
             await Clients.Group(groupName).SendAsync("UpdatedGroup", group);
 
-            var messages = await _messageRepository.
+            var messages = await _unitOfWork.MessageRepository.
                 GetMessageThread(Context.User.GetUserName(), otherUser);
+
+            if (_unitOfWork.HasChanges()) await _unitOfWork.Complete();
 
             await Clients.Caller.
                 SendAsync("ReceiveMessageThread", messages);
@@ -61,8 +61,8 @@ namespace API.SignalR
             if (username == createMessageDto.RecipientUserName.ToLower())
                 throw new HubException("You cannot send a message to yourself");
             
-            var sender = await _userRepository.GetUserByUserNameAsync(username);
-            var recipient = await _userRepository.GetUserByUserNameAsync(createMessageDto.RecipientUserName);
+            var sender = await _unitOfWork.UserRepository.GetUserByUserNameAsync(username);
+            var recipient = await _unitOfWork.UserRepository.GetUserByUserNameAsync(createMessageDto.RecipientUserName);
 
             if (recipient == null) throw new HubException("Not found user");
 
@@ -76,7 +76,7 @@ namespace API.SignalR
             };
 
             var groupName = GetGroupName(sender.UserName, recipient.UserName);               
-            var group = await _messageRepository.GetMessageGroup(groupName);
+            var group = await _unitOfWork.MessageRepository.GetMessageGroup(groupName);
             if (group.Connections.Any(x => x.UserName == recipient.UserName))
             {
                 message.DateRead = DateTime.UtcNow;
@@ -90,33 +90,33 @@ namespace API.SignalR
                 }
             }
 
-            _messageRepository.AddMessage(message);
+            _unitOfWork.MessageRepository.AddMessage(message);
 
-            if (await _messageRepository.SaveAllAsync()) 
+            if (await _unitOfWork.Complete()) 
             {
                 await Clients.Group(groupName).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
             }
         }
         private async Task<Group> AddToGroup(string groupName)
         {
-            var group = await _messageRepository.GetMessageGroup(groupName);
+            var group = await _unitOfWork.MessageRepository.GetMessageGroup(groupName);
             var connection = new Connection(Context.ConnectionId, Context.User.GetUserName());
             if (group == null)
             {
                 group = new Group(groupName);
-                _messageRepository.AddGroup(group);
+                _unitOfWork.MessageRepository.AddGroup(group);
             }
             group.Connections.Add(connection);
-            if (await _messageRepository.SaveAllAsync()) return group;
+            if (await _unitOfWork.Complete()) return group;
             throw new HubException("Failed to join group");
         }
 
         private async Task<Group> RemoveFromMessageGroup()
         {
-            var group = await _messageRepository.GetGroupForConnection(Context.ConnectionId);
+            var group = await _unitOfWork.MessageRepository.GetGroupForConnection(Context.ConnectionId);
             var connection = group.Connections.FirstOrDefault(x => x.ConnectionId == Context.ConnectionId);
-            _messageRepository.RemoveConnection(connection);
-            if ( await _messageRepository.SaveAllAsync()) return group;
+            _unitOfWork.MessageRepository.RemoveConnection(connection);
+            if ( await _unitOfWork.Complete()) return group;
             throw new HubException("Failed to remove from group");
         }
         private string GetGroupName(string caller, string other)
